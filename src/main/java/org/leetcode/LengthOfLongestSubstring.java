f0dd30a4a32a5ac1e20f460234cfd3f0303c1b25
Anand Raina <anandraina95@gmail.com>
2023-05-25 17:30:25 +0530

    LeetCode #1

---
+++ src/main/java/org/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,20 @@
+package org.leetcode;
+
+import java.util.LinkedHashSet;
+
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+        String s = "abcdedgxcvbnm";
+        LinkedHashSet<Character> linkedHashSet = new LinkedHashSet<>();
+        int max = 0;
+        int left = 0;
+        for(int right = 0; right < s.length(); right++) {
+            while(!linkedHashSet.add(s.charAt(right))) {
+                linkedHashSet.remove(s.charAt(left++));
+            }
+            max = Math.max(max, (right - left) + 1);
+        }
+
+        System.out.println("Length of Longest Substring = " + max);
+    }
+}
